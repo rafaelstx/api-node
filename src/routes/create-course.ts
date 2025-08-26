6a58e20ad024a283194e4f69f8fefffb50d9aa08
Rafael <rafael.s.teixeira@unesp.br>
2025-08-26 16:48:17 -0300

    v4 - Auth - Hash - Login (preDeploy)

--- src/routes/create-course.ts
+++ src/routes/create-course.ts
@@ -2,32 +2,31 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { db } from '../database/client.ts'
 import { courses } from '../database/schema.ts'
 import z from 'zod'
+import { checkRequestJWT } from './hooks/check-request-jwt.ts'
+import { checkUserRole } from './hooks/check-user-role.ts'
 
 export const createCourseRoute: FastifyPluginAsyncZod = async (server) => {
   server.post('/courses', {
+    preHandler: [
+      checkRequestJWT,
+      checkUserRole('manager'),
+    ],
     schema: {
       tags: ['courses'],
       summary: 'Create a course',
       body: z.object({
         title: z.string().min(5, 'Título precisa ter 5 caracteres'),
-        // 1. Adicione a validação para o description
-        description: z.string().nullable(), // .nullable() torna o campo opcional
       }),
       response: {
-        201: z.object({ courseId: z.string() }).describe('Curso criado com sucesso!') // Ajustado para z.string() se o ID for texto
+        201: z.object({ courseId: z.uuid() }).describe('Curso criado com sucesso!')
       }
     },
   }, async (request, reply) => {
-    // 2. Extraia o title E o description do corpo da requisição
-    const { title, description } = request.body
+    const courseTitle = request.body.title
   
     const result = await db
       .insert(courses)
-      // 3. Passe ambos os valores para o banco de dados
-      .values({ 
-        title, 
-        description 
-      })
+      .values({ title: courseTitle })
       .returning()
   
     return reply.status(201).send({ courseId: result[0].id })
